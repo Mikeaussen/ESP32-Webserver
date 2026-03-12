7da10c01df140ba022a8ba547181517d4930ec83
Mike <m.aussendorf@gastromatix.de>
2026-03-12 20:48:10 +0100

    Auswahl für Rezeptverzeichnis

--- lib/webinterface/webserver.h
+++ lib/webinterface/webserver.h
@@ -1,10 +1,24 @@
 #include "Arduino.h"
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+
+struct WebProfileConfig
+{
+  String id;
+  String rezeptPath;
+  String productPath;
+};
+
 void configureWebServer();
+void configureWebServer(const String &profileId);
 String humanReadableSize(const size_t bytes);
-String processor(const String& var) ;
+String processor(const String& var);
 String listFiles(bool ishtml,const char *path);
 bool initFileSystem();
+
+WebProfileConfig getActiveWebProfileConfig();
+bool setActiveWebProfile(const String &profileId);
+String getActiveProfileId();
+
 // handles uploads to the filserver
 void handleUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final);
